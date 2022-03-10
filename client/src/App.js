edc3fd1f900ae2674a8a0b21e435b2456108a56d
Vi Vu <thuyvi2309@gmail.com>
2022-03-10 11:45:29 -0600

    Added tiny piano to main page

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,23 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Synthesizer from "./components/pages/synthesizer";
+
 import './App.css';
 
 class App extends Component {
-state = {
-    data: null
-  };
-
-  componentDidMount() {
-      // Call our fetch function below once the component mounts
-    this.callBackendAPI()
-      .then(res => this.setState({ data: res.express }))
-      .catch(err => console.log(err));
-  }
-    // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
-  callBackendAPI = async () => {
-    const response = await fetch('/express_backend');
-    const body = await response.json();
-
-    if (response.status !== 200) {
-      throw Error(body.message) 
-    }
-    return body;
-  };
 
   render() {
     return (
-      <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">Welcome to React</h1>
-      </header>
-      <p className="App-intro">{this.state.data}</p>
-    </div>
-      );
+      <>
+        <Router>
+          <div className="Pages">
+            <Routes>
+            <Route path='/' element={<Synthesizer/>} />
+            </Routes>
+          </div>
+        </Router>
+      </>
+    );
   }
 }
 
