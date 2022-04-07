f10a77a7ffbb2f20a68610ffbae40d1af5977176
Vi Vu <49687631+vivu23@users.noreply.github.com>
2022-04-07 10:59:57 -0500

    Vi (#1)
    * testing webhook
    
    * Testing webhook
    
    * connect DB to backend
    
    * Connect DB to backend
    
    * Added tiny piano to main page
    
    * Login page and Navbar is created
    
    * Navbar is done
    
    * added better piano
    
    * got the keys to play sounds
    
    * Got choosing waveform to work
    
    * Keyboard works now
    
    * Fix database connection, login/signup not working
    
    * Login and SignUp works

---
+++ client/src/components/pages/profile.js
@@ -0,0 +1,10 @@
+import React from "react";
+import '../css/Main.css';
+
+export const Profile = () => {
+    return (
+     <h1>HELLO!</h1>
+    );
+  };
+  
+export default Profile;
--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 5000;
+const dbConnection = require("./databases/connect");
+const user = require("./routes/User");
 
-// console.log that your server is up and running
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.use(express.json());
 
-// create a GET route
-app.get('/express_backend', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+app.use('/login', user);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+
+app.listen(port, (req, res) => {
+  dbConnection;
+  console.log(`Server listening on port: ${port}`);
 });
